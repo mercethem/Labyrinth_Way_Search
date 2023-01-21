5aa258924a12661ade54e5606b7fe9a2bf7d8633
Ethem MERÇ <114696279+mercethem@users.noreply.github.com>
2023-01-21 13:39:18 +0300

    Code cleaned and reformatted

--- src/main.java
+++ src/main.java
@@ -5,9 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class Main {
-    private static int dx;
-
+public class main {
     public static void main(String[] args) {
 
         Integer[][] arrayLabyrinth = new Integer[20][20];
@@ -17,15 +15,15 @@ public class Main {
         ArrayList<Integer> dataList = new ArrayList<Integer>();
 
         try {
-            File readedFile = new File("E:\\Labyrinth\\src\\data.txt");     // Dosyanın okunacağı konumu belirtir
-            Scanner scan = new Scanner(readedFile);                                 // Dosyadan okumak adına belirtilmiştir
+            File readedFile = new File("E:\\Labyrinth\\src\\data.txt");
+            Scanner scan = new Scanner(readedFile);
             int i = 0;
-            while (scan.hasNextLine()) {             //Döngü vasıtasıyla karakter karakter String taraması yaparak okuyacaktır
-                String data = scan.nextLine();       //ve okunan txt yapısının tamamını string alacağı için bu yapıları
-                for (int j = 0; j < 20; j++) {       //teker teker char olarak tanımayacak bir döngü oluşturulur ve bu döngü
-                    int x = data.charAt(j);          //char okunmasından dolayı ascii tablosunun içereceği için bunları int tanımlamak adına
-                    if (x == 49) {                   //öncelikle integer olarak düzenleyecek ve bu düzenleme üzerinden sonra sırayla
-                        x = 1;                       //2 boyutlu bir dizi oluşturup bunun içesinde atacaktır.
+            while (scan.hasNextLine()) {
+                String data = scan.nextLine();
+                for (int j = 0; j < 20; j++) {
+                    int x = data.charAt(j);
+                    if (x == 49) {
+                        x = 1;
                     } else if (x == 48) {
                         x = 0;
                     } else x = 9;
@@ -40,28 +38,6 @@ public class Main {
             e.printStackTrace();
         }
 
-        //emre
-        ArrayList<Integer> temp = new ArrayList<>();
-        for (int i = 0; i < arrayLabyrinth.length; i++) {
-            temp.add(i);
-
-            for (int j = 0; j < arrayLabyrinth[0].length; j++) {
-                if (arrayLabyrinth[i][j] == 9) {
-                    j = Integer.MAX_VALUE - 1;
-                    i = Integer.MAX_VALUE - 1;
-                    break;
-                }
-                if (arrayLabyrinth[i][j] == 1) {
-                    System.out.printf("%s,%s ", i, j);
-                    temp.add(j);
-                }
-            }
-            System.out.println();
-            temp.add(-1);
-        }
-
-        // -emre
-        // BFS algoritmasını kullanarak çıkışı bulun
         Queue<Integer> qx = new LinkedList<>();
         Queue<Integer> qy = new LinkedList<>();
         qx.add(0);
@@ -74,7 +50,7 @@ public class Main {
 
             if (arrayLabyrinth[x][y] == 9) {
                 // Çıkış bulundu
-                System.out.println("LABİRET ÇÖZÜLDÜ!" + " \n" + "x : " + x + " " + " y:  " + y);
+                System.out.println("LABİRENT ÇÖZÜLDÜ!" + " \n" + "x : " + x + " " + " y:  " + y);
                 break;
             }
             for (int i = 0; i < 4; i++) {
@@ -88,11 +64,11 @@ public class Main {
                 }
             }
         }
+
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 20; j++) {
                 if (arrayLabyrinth[i][j] == 1) {
                     System.out.print(" 1 ");
-
                 } else if (arrayLabyrinth[i][j] == 9) {
                     System.out.println(" 9 ");
                 } else {
